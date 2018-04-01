dcf13b32b7ff333b6ce5fd1a485bc0a4c43defb7
wooyangee@gmail.com <wooyangee@gmail.com>
2018-04-01 06:32:30 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,5 +27,5 @@ function destructivelyRemoveElementFromEndOfArray(chocolateBars) {
 }
 
 function removeElementFromEndOfArray(chocolateBars) {
-  return chocolateBars.slice(0, chocolateBars.length - 1);
+  return chocolateBars.slice(0, chocolateBars.length-1);
 }
