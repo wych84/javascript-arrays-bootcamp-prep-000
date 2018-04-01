8974822f2cc2d2817a4c1b4c0efd9d5f9357e14c
wooyangee@gmail.com <wooyangee@gmail.com>
2018-04-01 06:20:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,19 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+var element = "M&M"
+
+function addElementToBeginningOfArray(chocolateBars, element) {
+  return ["element", ...chocolateBars];
+}
+
+function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
+  chocolateBars.unshift(element);
+  return chocolateBars;
+}
+
+function accessElementInArray(chocolateBars) {
+  return chocolateBars[2];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
+  return chocolateBars.shift();
+}
