6f3bbe370b7444394ada675b68e6d344de7b39da
wooyangee@gmail.com <wooyangee@gmail.com>
2018-04-01 06:37:34 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -10,6 +10,14 @@ function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
   return chocolateBars;
 }
 
+function addElementToEndOfArray (chocolateBars, element){
+  return [...chocolateBars, element];
+}
+function destructivelyAddElementToEndOfArray(chocolateBars, element){
+  chocolateBars.push(element);
+  return chocolateBars;
+}
+
 function accessElementInArray(chocolateBars) {
   return chocolateBars[2];
 }
